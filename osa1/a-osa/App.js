6c376de70f0f1d75ecff338e3a8f7da431db088e
Petter <petterkauppi@icloud.com>
2022-05-25 07:11:46 +0300

    tt

---
+++ osa1/a-osa/App.js
@@ -0,0 +1,44 @@
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+  
+    const Header = (props) => {
+        return (<h1>props.course</h1>)
+    }
+
+
+    const Part = (props) => {
+        return (
+            <p>props.part props.exercise</p>
+        )
+    }
+    const Content = () => {
+        return (
+            <div>
+                <Part part={part1} exercise={exercises1} />
+                <Part part={part1} exercise={exercises1} />
+                <Part part={part1} exercise={exercises1} />
+            </div>
+        )
+    }
+
+    const Total = () =>{
+        return (
+            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        )
+    }
+    return (
+      <div>
+        <Header course={course} />
+        <Content />
+        <Total />
+      </div>
+    )
+  }
+  
+  export default App
